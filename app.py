d88db8df3dad850354b1414037f241f214a26ace
chrisschimkat <79619747+chrisschimkat@users.noreply.github.com>
2023-03-31 15:24:05 +0200

    updated

--- app.py
+++ app.py
@@ -3,11 +3,28 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import io
+from PIL import Image
+import requests
+
 st.title("Metrics Correlation")
 st.markdown("See example input file [here](https://github.com/chrisschimkat/metriccorrelation/blob/main/Book1.csv)")
+# Add a button for loading the sample CSV
+if st.button('Load Sample CSV'):
+    sample_csv_url = 'https://raw.githubusercontent.com/chrisschimkat/metriccorrelation/main/Book1.csv'
+    uploaded_file = requests.get(sample_csv_url).content
+
+# Add a download link for the example input file
+example_csv_link = '[here](https://raw.githubusercontent.com/chrisschimkat/metriccorrelation/main/Book1.csv?raw=true)'
+st.markdown(f"See example input file {example_csv_link}")
+
 uploaded_file = st.file_uploader("Upload a CSV file:", type=['csv'])
+
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, parse_dates=['Date'], dayfirst=True)
+    df = pd.read_csv(uploaded_file)
+    df.columns = [col.capitalize() for col in df.columns]
+    df.columns = [col.strip() for col in df.columns]  # Remove spaces in column names
+    df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
     df.set_index('Date', inplace=True)
     def apply_decay(data, decay_rate):
         decayed_data = data.copy()
@@ -18,13 +35,21 @@ if uploaded_file is not None:
     decayed_df = apply_decay(df, decay_rate)
     decayed_correlations = decayed_df.corr()
     st.header("Correlation matrix")
-
     fig, ax = plt.subplots(figsize=(10, 8))
-    sns.heatmap(decayed_correlations, annot=True, fmt='.2f', cmap='plasma', vmin=-1, vmax=1, ax=ax)
     sns.heatmap(decayed_correlations, annot=True, fmt='.2f', cmap='plasma_r', vmin=-1, vmax=1, ax=ax)
     ax.set_title('Correlations with decay effect')
     st.pyplot(fig)
-
+    # Save the plot to a buffer
+    buffer = io.BytesIO()
+    fig.savefig(buffer, format='png')
+    buffer.seek(0)
+    # Convert the buffer to bytes
+    img_bytes = buffer.getvalue()
+    buffer.close()
+    # Export heatmap plot to PNG
+    if st.button('Export heatmap plot to PNG'):
+        st.download_button("Download heatmap plot", img_bytes, "heatmap_plot.png", "image/png")
+    
     st.header("Top 10 correlations:")
     
     # Mask the lower triangle of the correlation matrix
@@ -32,3 +57,31 @@ if uploaded_file is not None:
     correlations_upper_triangle = decayed_correlations.where(mask)
     top_10_correlations = correlations_upper_triangle.stack().nlargest(10)
     st.write(top_10_correlations.to_frame('Correlation'))
+    # Time series chart
+    st.header("Time series chart for selected metrics")
+    st.markdown("Select two metrics to see how they compare over time. Use this to help with identifying the timeframe between cause and effect.")
+    selected_metrics = st.multiselect("Select two metrics to plot:", options=df.columns, default=df.columns[:2].tolist())
+    if len(selected_metrics) == 2:
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.plot(decayed_df[selected_metrics[0]], label=selected_metrics[0])
+        ax.set_ylabel(selected_metrics[0], fontsize=12)
+        ax2 = ax.twinx()
+        ax2.plot(decayed_df[selected_metrics[1]], color='orange', label=selected_metrics[1])
+        ax2.set_ylabel(selected_metrics[1], fontsize=12)
+        ax.set_xlabel('Date', fontsize=12)
+        ax.legend(loc='upper left')
+        ax2.legend(loc='upper right')
+        st.pyplot(fig)
+        
+        # Save the plot to a buffer
+        buffer = io.BytesIO()
+        fig.savefig(buffer, format='png')
+        buffer.seek(0)
+        # Convert the buffer to bytes
+        img_bytes = buffer.getvalue()
+        buffer.close()
+        # Export time series plot to PNG
+        if st.button('Export time series plot to PNG'):
+            st.download_button("Download time series plot", img_bytes, "time_series_plot.png", "image/png")
+    else:
+        st.warning("Please select exactly two metrics.")
